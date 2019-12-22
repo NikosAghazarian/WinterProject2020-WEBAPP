3245f0b8b4c45b5845def995cc7b7b01c1f36043
Nikos Aghazarian <odst46@gmail.com>
2019-12-22 14:59:28 -0800

    added routes and component generation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewerComponent } from './viewer/viewer.component';
+import { EditorComponent } from './editor/editor.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'editor/:type', component: EditorComponent },
+  { path: 'viewer/:type', component: ViewerComponent },
+  { path: '', redirectTo: '/viewer/1', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    ViewerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
